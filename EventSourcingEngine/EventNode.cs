7e2c3f77d870015f0342f441626c0eaeb199cf15
przemek <Brodeon@gmail.com>
2025-12-20 12:02:56 +0100

    fix genrics

--- EventSourcingEngine/EventNode.cs
+++ EventSourcingEngine/EventNode.cs
@@ -1,6 +1,8 @@
 namespace EventSourcingEngine;
 
-public record EventNode<TState, TEvent>(HashSet<string> HandlesEvents, Type Executor, HashSet<string> ProducesEvents, List<EventNode<TState, TEvent>> NextExecutors);
+public record EventNode<TState, TEvent>(HashSet<string> HandlesEvents, Type Executor, HashSet<string> ProducesEvents, List<EventNode<TState, TEvent>> NextExecutors)
+    where TState : new()
+    where TEvent : Event;
 
 internal record EventNodeInst<TState, TEvent>(INodeExecutor<TState, TEvent> Executor, List<EventNodeInst<TState, TEvent>> NextExecutors) 
     where TState : new()
