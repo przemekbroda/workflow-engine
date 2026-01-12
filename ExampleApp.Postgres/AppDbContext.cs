aa7be954655e5243d835311b23b8b6352aa8cbc2
Przemek <przemyslaw.broda.95@gmail.com>
2026-01-12 16:32:36 +0100

    test

--- ExampleApp.Postgres/AppDbContext.cs
+++ ExampleApp.Postgres/AppDbContext.cs
@@ -25,5 +25,6 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         modelBuilder.Entity<ProcessRequest>()
             .HasIndex(e => e.LastModifiedAt);
         
+        
     }
 }
